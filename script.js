d1411a37c64b785812f452b2b085d19dda4742b4
IrtizaShahbaz <irtizashahbaz22@gmail.com>
2024-01-09 20:54:24 +0500

    add complete files

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-const inpBox = document.getElementById("date");
-inpBox.max = new Date().toISOString().split("T")[0]; 
+const userInp = document.getElementById("date");
+userInp.max = new Date().toISOString().split("T")[0]; 
 
 function calculateAge(){
+    let birthDate = new Date(userInp.value);
+
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
+
+    let today = new Date();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+
+    y3 = y2 -y1;
     
+    if(m2 >= m1){
+        m3 = m2 - m1
+    }else{
+        y3--;
+        m3 = 12 + m2 -m1;
+    }
+
+    if(d2 >= d1){
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        d3 = getDays(y1, m1) + d2 - d1;
+    }
+    if(m3 < 0){
+        m3 = 11;
+        y3--;
+    }
+    document.getElementById("day").innerText = d3;
+    document.getElementById("month").innerText = m3;
+    document.getElementById("year").innerText = y3;
 }
+function getDays(year, month){
+    return new Date(year, month, 0).getDate()
+}
+
